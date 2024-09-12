696c66a4a40841c47a943356491a2b6ea0a6b513
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-09-12 16:21:36 +0800

    Add price comparison and total calculation logic

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ let hatPrice = 100;
 console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
+
+if (hatPrice == bootsPrice) {
+    console.log("Prices are the same");
+} else {
+    console.log("Prices are different");
+}
+
+let totalPrice = hatPrice + bootsPrice;
+console.log(`Total Price: ${totalPrice}`);
