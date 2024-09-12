a801ea846ac05864d93643d03214bb495edd48c5
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-09-12 17:25:59 +0800

    Refactor array destructuring and sorting logic

--- index3.js
+++ index3.js
@@ -1,3 +1,11 @@
 let names = ["Hat", "Boots", "Gloves"];
 let [one, two] = names;
 console.log(`First: ${one}, Second: ${two}`);
+
+let [, , three] = names;
+console.log(`Third: ${three}`);
+
+
+let prices = [100, 120, 50.25];
+[, ...highest] = prices.sort((a, b) => a - b);
+highest.forEach(price => console.log(`High price: ${price}`));
