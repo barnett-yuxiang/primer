30fb517a54a518268f0bfb2baaa8832e0bf04908
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-09-12 22:54:31 +0800

    Refactor code to remove unused files and improve code organization

---
+++ index-4.js
@@ -0,0 +1,28 @@
+let hat = {
+    name: "Hat",
+    price: 100,
+}
+
+let boots = {
+    name: "Boots",
+    price: "100",
+}
+
+let gloves = {
+    productName: "Gloves",
+    price: 40,
+}
+
+gloves.name = gloves.productName;
+delete gloves.productName;
+gloves.price = 20;
+
+//let sumPrices = (...numbers) => numbers.reduce((total, val) => total + (Number.isNaN(val)) ? 0 : Number(val))
+let sumPrices = (...numbers) => numbers.reduce((total, val) => total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
+let totalPrice = sumPrices(hat.price, boots.price, gloves.price);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+
+let otherHat = { ...hat };
+console.log(`Spread: ${otherHat.name}, ${otherHat.price}`);
+
+
---
+++ index-5.js
@@ -0,0 +1,33 @@
+
+let hat = {
+    name: "Hat",
+    _price: 100,
+    priceIncTax: 100 * 1.2,
+    set price(newPrice) {
+        this._price = newPrice;
+        this.priceIncTax = this._price * 1.2;
+    },
+    get price() {
+        return this._price;
+    }
+}
+
+
+
+let boots = {
+    name: "Boots",
+    _price: "100",
+    get priceIncTax() {
+        return Number(this._price) * 1.2;
+    }
+}
+
+console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
+
+hat.price = 120;
+console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
+
+console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
+
+boots.price = "120";
+console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
