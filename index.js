046a35009a777aad7dce10fd89200df23579390a
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-09-12 16:40:02 +0800

    Refactor variable type and price comparison logic

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
-if (hatPrice == bootsPrice) {
+if (hatPrice === bootsPrice) {
     console.log("Prices are the same");
 } else {
     console.log("Prices are different");
 }
 
-let totalPrice = hatPrice + bootsPrice;
+let totalPrice = Number(hatPrice) + Number(bootsPrice);
 console.log(`Total Price: ${totalPrice}`);
 
 let myVariable = "Adam";
@@ -17,3 +17,12 @@ console.log(`Type: ${typeof myVariable}`);
 
 myVariable = 100;
 console.log(`Type: ${typeof myVariable}`);
+
+let firstCity;
+let secondCity = firstCity || "London";
+console.log(`City: ${secondCity}`);
+
+let taxRate;
+console.log(`Tax rate: ${taxRate || 10}%`);
+taxRate = 0;
+console.log(`Tax rate: ${taxRate || 10}%`);
