3b2bb226735228d588dbd071502db9c6841e98ee
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-09-12 17:07:12 +0800

    Refactor sumPrices function to accept variable number of arguments

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
-function sumPrices(first, second, third = 0) {
-    return first + second + third;
+function sumPrices(...numbers) {
+    return numbers.reduce((total, val) => total + Number(val), 0);
 }
 
 let totalPrice = sumPrices(hatPrice, bootsPrice);
