aba66c8893e22720f9dc66569aaeb7f9bad9b2a4
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-09-12 22:57:58 +0800

    Refactor code to improve price handling and add writeDetails function

--- index-5.js
+++ index-5.js
@@ -3,31 +3,33 @@ let hat = {
     name: "Hat",
     _price: 100,
     priceIncTax: 100 * 1.2,
+
     set price(newPrice) {
         this._price = newPrice;
         this.priceIncTax = this._price * 1.2;
     },
+
     get price() {
         return this._price;
+    },
+
+    writeDetails: function () {
+        console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`);
     }
 }
-
-
 
 let boots = {
     name: "Boots",
     _price: "100",
+
     get priceIncTax() {
         return Number(this._price) * 1.2;
     }
 }
 
-console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
-
+hat.writeDetails();
 hat.price = 120;
-console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
-
+hat.writeDetails();
 console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
-
 boots.price = "120";
 console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
