dbd58bd51395f23e0e69020e0f6f868feb926675
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-09-12 17:16:49 +0800

    Refactor sumPrices function to accept variable number of arguments and improve type handling

---
+++ index2.js
@@ -0,0 +1,13 @@
+let names = ["Hat", "Boots", "Gloves"];
+let prices = [];
+prices.push(100);
+prices.push("100");
+prices.push(50.25);
+console.log(`First Item: ${names[0]}: ${prices[0]}`);
+
+let sumPrices = (...numbers) => numbers.reduce((total, val) => total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
+let totalPrice = sumPrices(...prices);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+
+let combinedArray = [...names, ...prices];
+combinedArray.forEach(element => console.log(`Combined Array Element: ${element}`));
